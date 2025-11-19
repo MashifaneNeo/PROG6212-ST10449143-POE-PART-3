84e2f6720821aa2451c84c0360e6f03c5b029e83
Neo Ya Gao Mashifane <ST10449143@imconnect.edu.za>
2025-11-19 20:23:20 +0200

    Created user model to allow for HR to manage users

---
+++ PROG6212 ST10449143 POE PART 3/Models/User.cs
@@ -0,0 +1,35 @@
+﻿// Models/User.cs
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PROG6212_ST10449143_POE_PART_1.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal HourlyRate { get; set; }
+
+        [StringLength(20)]
+        public string EmployeeId { get; set; }
+
+        [StringLength(100)]
+        public string Department { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = true;
+
+        // Navigation property
+        public virtual ICollection<Claim> Claims { get; set; } = new List<Claim>();
+    }
+}
