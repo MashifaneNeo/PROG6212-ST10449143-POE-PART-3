c49153dc0374b6557f922e0b0ec4740b248d429b
Neo Ya Gao Mashifane <ST10449143@imconnect.edu.za>
2025-11-19 20:51:02 +0200

    altered program to run with identity and Identity.EntityFrameworkCore

--- PROG6212 ST10449143 POE PART 3/Program.cs
+++ PROG6212 ST10449143 POE PART 3/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using PROG6212_ST10449143_POE_PART_1.Models;
 using PROG6212_ST10449143_POE_PART_1.Services;
 
@@ -10,7 +12,27 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add Identity
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+});
+
 builder.Services.AddScoped<IClaimService, DatabaseClaimService>();
+builder.Services.AddScoped<IHRService, HRService>();
 
 var app = builder.Build();
 
@@ -25,10 +47,47 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+
+    string[] roleNames = { "HR", "Lecturer", "Coordinator" };
+    foreach (var roleName in roleNames)
+    {
+        if (!await roleManager.RoleExistsAsync(roleName))
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+
+    var hrUser = new User
+    {
+        FirstName = "HR",
+        LastName = "Admin",
+        UserName = "hr@university.ac.za",
+        Email = "hr@university.ac.za",
+        HourlyRate = 0,
+        EmployeeId = "HR001",
+        Department = "Human Resources"
+    };
+
+    var hrUserExists = await userManager.FindByEmailAsync(hrUser.Email);
+    if (hrUserExists == null)
+    {
+        var createHR = await userManager.CreateAsync(hrUser, "TempPassword123!");
+        if (createHR.Succeeded)
+        {
+            await userManager.AddToRoleAsync(hrUser, "HR");
+        }
+    }
+}
 
 app.Run();
